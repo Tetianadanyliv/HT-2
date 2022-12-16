7314a99d01965c483ef17a3919b252f184d1f28e
Tetianadanyliv <tanyadanuliv@gmail.com>
2022-12-16 15:06:47 +0200

    HT-2

---
+++ js/script.js
@@ -0,0 +1,38 @@
+let numberN = Number(prompt("Please, enter integer value N"));
+
+if (Number.isInteger(numberN)) {
+} else if (!Number.isInteger(numberN)) {
+    alert ('The value is not suitable!');
+    let numberN = Number(prompt("Please, enter integer value N"));
+}
+else (Number.isInteger(numberN));
+
+
+
+let numberM = Number(prompt("Please, enter integer value M"));
+if (Number.isInteger(numberM)) {
+} else if (!Number.isInteger(numberM)) {
+    alert ('The value is not suitable!');
+    let numberN = Number(prompt("Please, enter integer value M"));
+}
+else (Number.isInteger(numberM));
+
+
+
+
+const missEvenNumber = confirm("Чи потрібно пропускати парні числа?");
+
+
+    let sum = 0;
+    for (let i = numberN; i <= numberM; i++) {
+        if (missEvenNumber);
+        if ((i % 2)  ==0) {
+            sum = sum;
+        }
+        else (i % 2 ==!0)
+            sum = sum + i;
+        }
+    console.log (sum)
+
+
+  
